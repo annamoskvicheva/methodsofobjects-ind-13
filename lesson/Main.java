fe8f1880bed92f046e9e7bcc3cde10da9e06e768
annamoskviceva <Annab.spb@mail.ru>
2023-07-10 22:52:27 +0300

    created paketage

---
+++ lesson/Main.java
@@ -0,0 +1,21 @@
+package lesson;
+
+public class Main {
+    public static void main(String[] args) {
+        Author dostoevsky = new Author("Фёдор","Достоевский");
+        Book prestuplenieNakazanie = new Book("Преступление и наказание", dostoevsky, 1866);
+        Author gogol = new Author("Николай","Гоголь");
+        Book mertveyDushi = new Book("Мёртвые души", gogol,1835);
+
+        System.out.println(dostoevsky);
+        System.out.println(prestuplenieNakazanie);
+
+        System.out.println(gogol);
+        System.out.println(mertveyDushi);
+
+        Author dostoevsky2 = new Author("Фёдор","Достоевский");
+        System.out.println(dostoevsky.equals(dostoevsky2));
+        System.out.println(dostoevsky.hashCode());
+        System.out.println(dostoevsky2.hashCode());
+    }
+}
